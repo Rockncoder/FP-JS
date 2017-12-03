2c2a80f9942188eae34214a3be18b9f027a68f13
rockncoder <rockncoder@gmail.com>
2017-12-03 15:05:15 -0800

    Updates the code to full ES6 with arrow functions and no mutable variables after fizzbuzz1

--- src/function-factory.js
+++ src/function-factory.js
@@ -7,22 +7,29 @@
   'use strict';
 
   // a simple function factory
-  function addN(n) {
+  function oldStyleAddN(n) {
     // a function is returned
     return function (x) {
       return n + x;
     }
   }
 
+  // a simple function factory
+  const addN = n => x => n + x;
+
   // let's create two functions, add10 and add50
   const add10 = addN(10);
   const add50 = addN(50);
+  const add100 = oldStyleAddN(100);
+
   // they don't interfere with each other or share variables or state
-  console.info(add10(1));
-  console.info(add50(1));
-  console.info(add10(5));
-  // 11
-  // 51
-  // 15
+  console.info('add10(1) = ', add10(1));
+  console.info('add50(1) = ', add50(1));
+  console.info('add10(5) = ', add10(5));
+  console.info('add100(50) = ', add100(50));
+  // add10(1) =  11
+  // add50(1) =  51
+  // add10(5) =  15
+  // add100(50) =  150
 
 }());
